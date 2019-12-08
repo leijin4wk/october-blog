f2683a2864a40c0fd279aae0cb42af30801b0be5
leijin4wk <leijin4wk@163.com>
2019-12-08 11:08:52 +0800

    init

--- src/main/java/com/octlr/blog/component/InitDBData.java
+++ src/main/java/com/octlr/blog/component/InitDBData.java
@@ -32,7 +32,7 @@ public class InitDBData implements ApplicationRunner {
     private OssDto  ossDto;
     @Override
     public void run(ApplicationArguments args) {
-        readProperty(ossConfig.getConfigPath());
+        OssDto ossDto= readProperty(ossConfig.getConfigPath());
         Classify classify = Classify.builder()
                 .build();
         classifyRepository.save(classify);
@@ -40,26 +40,28 @@ public class InitDBData implements ApplicationRunner {
                 .classifyId(classify.getId())
                 .build();
         articleRepository.save(article);
-        uploadFile(ossConfig.getDbPath(),ossConfig.getDbName());
+        uploadFile(ossDto,ossConfig.getDbPath(),ossConfig.getDbName());
         log.info("初始化数据库！");
     }
-    private void readProperty(String configName) {
+    private OssDto readProperty(String configName) {
         File file = new File(configName);
         try {
             String str = FileUtils.readFileToString(file, "utf-8");
             Properties props = new Properties();
             props.load(new StringReader(str));
-            ossDto= OssDto.builder().endpoint(props.getProperty("endpoint"))
+            OssDto ossDto= OssDto.builder().endpoint(props.getProperty("endpoint"))
                     .accessKeyId(props.getProperty("accessKeyId"))
                     .accessKeySecret(props.getProperty("accessKeySecret"))
                     .bucketName(props.getProperty("bucketName")).build();
             log.info(ossDto.toString());
+            return ossDto;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return null;
     }
 
-    private void uploadFile(String uploadFile,String uploadName){
+    private void uploadFile(OssDto ossDto,String uploadFile,String uploadName){
         OSS ossClient = new OSSClientBuilder().build(ossDto.getEndpoint(),
                 ossDto.getAccessKeyId(),
                 ossDto.getAccessKeySecret());
@@ -71,6 +73,5 @@ public class InitDBData implements ApplicationRunner {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
     }
 }
---
+++ src/main/java/com/octlr/blog/utils/OssUtils.java
@@ -0,0 +1,45 @@
+package com.octlr.blog.utils;
+
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectResult;
+import com.octlr.blog.dto.OssDto;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.util.Properties;
+@Slf4j
+public class OssUtils {
+    public static OssDto readProperty(String configName) {
+        File file = new File(configName);
+        try {
+            Properties props = new Properties();
+            String str = FileUtils.readFileToString(file, "utf-8");
+            props.load(new StringReader(str));
+            OssDto ossDto= OssDto.builder().endpoint(props.getProperty("endpoint"))
+                    .accessKeyId(props.getProperty("accessKeyId"))
+                    .accessKeySecret(props.getProperty("accessKeySecret"))
+                    .bucketName(props.getProperty("bucketName")).build();
+            log.info(ossDto.toString());
+            return ossDto;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void uploadFile(OssDto ossDto,String uploadFile,String uploadName){
+        InputStream inputStream = null;
+        try {
+            OSS ossClient = new OSSClientBuilder().build(ossDto.getEndpoint(),
+                    ossDto.getAccessKeyId(),
+                    ossDto.getAccessKeySecret());
+            inputStream = new FileInputStream(uploadFile);
+            PutObjectResult putObjectResult= ossClient.putObject(ossDto.getBucketName(),uploadName, inputStream);
+            ossClient.shutdown();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
