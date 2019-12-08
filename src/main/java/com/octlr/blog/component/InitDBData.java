cd671f727a3f54ae9b3b31e80bc30351b8cbb2b1
leijin4wk <leijin4wk@163.com>
2019-12-08 11:11:49 +0800

    init

--- src/main/java/com/octlr/blog/component/InitDBData.java
+++ src/main/java/com/octlr/blog/component/InitDBData.java
@@ -1,23 +1,17 @@
 package com.octlr.blog.component;
 
-import com.aliyun.oss.OSS;
-import com.aliyun.oss.OSSClientBuilder;
-import com.aliyun.oss.model.PutObjectResult;
 import com.octlr.blog.config.OssConfig;
 import com.octlr.blog.dto.OssDto;
 import com.octlr.blog.entity.Article;
 import com.octlr.blog.entity.Classify;
 import com.octlr.blog.repository.ArticleRepository;
 import com.octlr.blog.repository.ClassifyRepository;
+import com.octlr.blog.utils.OssUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
-
-import java.io.*;
-import java.util.Properties;
 
 
 @Component
@@ -29,10 +23,9 @@ public class InitDBData implements ApplicationRunner {
     private ClassifyRepository classifyRepository;
     @Autowired
     private OssConfig ossConfig;
-    private OssDto  ossDto;
     @Override
     public void run(ApplicationArguments args) {
-        OssDto ossDto= readProperty(ossConfig.getConfigPath());
+        OssDto ossDto= OssUtils.readProperty(ossConfig.getConfigPath());
         Classify classify = Classify.builder()
                 .build();
         classifyRepository.save(classify);
@@ -40,38 +33,7 @@ public class InitDBData implements ApplicationRunner {
                 .classifyId(classify.getId())
                 .build();
         articleRepository.save(article);
-        uploadFile(ossDto,ossConfig.getDbPath(),ossConfig.getDbName());
+        OssUtils.uploadFile(ossDto,ossConfig.getDbPath(),ossConfig.getDbName());
         log.info("初始化数据库！");
-    }
-    private OssDto readProperty(String configName) {
-        File file = new File(configName);
-        try {
-            String str = FileUtils.readFileToString(file, "utf-8");
-            Properties props = new Properties();
-            props.load(new StringReader(str));
-            OssDto ossDto= OssDto.builder().endpoint(props.getProperty("endpoint"))
-                    .accessKeyId(props.getProperty("accessKeyId"))
-                    .accessKeySecret(props.getProperty("accessKeySecret"))
-                    .bucketName(props.getProperty("bucketName")).build();
-            log.info(ossDto.toString());
-            return ossDto;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    private void uploadFile(OssDto ossDto,String uploadFile,String uploadName){
-        OSS ossClient = new OSSClientBuilder().build(ossDto.getEndpoint(),
-                ossDto.getAccessKeyId(),
-                ossDto.getAccessKeySecret());
-        InputStream inputStream = null;
-        try {
-            inputStream = new FileInputStream(uploadFile);
-            PutObjectResult putObjectResult= ossClient.putObject(ossDto.getBucketName(),uploadName, inputStream);
-            ossClient.shutdown();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 }
