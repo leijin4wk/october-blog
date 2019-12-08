606519008a2bc85c0d03ea9545303ced8a809300
leijin4wk <leijin4wk@163.com>
2019-12-08 21:10:04 +0800

    init

--- src/test/java/com/octlr/blog/BlogApplicationTests.java
+++ src/test/java/com/octlr/blog/BlogApplicationTests.java
@@ -1,13 +1,23 @@
 package com.octlr.blog;
 
+import com.octlr.blog.config.OssConfig;
+import com.octlr.blog.dto.OssDto;
+import com.octlr.blog.utils.OssUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.File;
 
 @SpringBootTest
 class BlogApplicationTests {
-
+    @Autowired
+    private OssConfig ossConfig;
     @Test
     void contextLoads() {
+        OssDto ossDto = OssUtils.readProperty(ossConfig.getLocalConfigPath());
+        File file = new File(ossConfig.getLocalArticlePath());
+
     }
 
 }
