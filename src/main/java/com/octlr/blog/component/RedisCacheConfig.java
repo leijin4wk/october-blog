1f0ed4bffe4c25efd328a9c011cc3a4cfe569014
leijin4wk <leijin4wk@163.com>
2019-12-29 11:56:59 +0800

    调试成功

--- src/main/java/com/octlr/blog/component/RedisCacheConfig.java
+++ src/main/java/com/octlr/blog/component/RedisCacheConfig.java
@@ -1,10 +1,17 @@
 package com.octlr.blog.component;
+import com.alibaba.fastjson.parser.ParserConfig;
 import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.cache.RedisCacheWriter;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 
 
@@ -12,13 +19,29 @@ import java.time.Duration;
 
 @Configuration
 @Slf4j
-public class RedisCacheConfig {
+public class RedisCacheConfig extends CachingConfigurerSupport {
     @Bean
-    public RedisCacheConfiguration redisCacheConfiguration(){
+    @Primary//当有多个管理器的时候，必须使用该注解在一个管理器上注释：表示该管理器为默认的管理器
+    public CacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        //初始化一个RedisCacheWriter
+        RedisCacheWriter redisCacheWriter = RedisCacheWriter.nonLockingRedisCacheWriter(connectionFactory);
         FastJsonRedisSerializer<Object> fastJsonRedisSerializer = new FastJsonRedisSerializer<>(Object.class);
-        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig();
-        configuration = configuration.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer)).entryTtl(Duration.ofSeconds(7200));
-        return configuration;
+        RedisSerializationContext.SerializationPair<Object> pair = RedisSerializationContext.SerializationPair.fromSerializer(fastJsonRedisSerializer);
+        RedisCacheConfiguration defaultCacheConfig = RedisCacheConfiguration.defaultCacheConfig().serializeValuesWith(pair);
+        //设置过期时间 7200秒
+        defaultCacheConfig = defaultCacheConfig.entryTtl(Duration.ofSeconds(7200));
+        //初始化RedisCacheManager
+        RedisCacheManager cacheManager = new RedisCacheManager(redisCacheWriter, defaultCacheConfig);
+        //设置白名单---非常重要********
+        /*
+        使用fastjson的时候：序列化时将class信息写入，反解析的时候，
+        fastjson默认情况下会开启autoType的检查，相当于一个白名单检查，
+        如果序列化信息中的类路径不在autoType中，
+        反解析就会报com.alibaba.fastjson.JSONException: autoType is not support的异常
+        可参考 https://blog.csdn.net/u012240455/article/details/80538540
+         */
+        ParserConfig.getGlobalInstance().addAccept("com.octlr.blog.entity");
+        ParserConfig.getGlobalInstance().addAccept("com.octlr.blog.vo");
+        return cacheManager;
     }
-
 }
